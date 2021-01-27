30a8c7ddf7022c4de7013303631ec30133b4d311
YellowBallPython <yellowballpython@gmail.com>
2021-01-27 16:02:05 -0600

    Added: JS and thumbnail
    Se agrega vanilla-JS para manejar el cambio de themes entre claro y oscuro.

---
+++ main.js
@@ -0,0 +1,17 @@
+const chk = document.getElementById("check");
+
+let logos = document.querySelectorAll("img");
+
+function logos_list(logos) {
+  console.log(logos);
+}
+
+logos_list(logos);
+
+chk.addEventListener("change", () => {
+  if (chk.checked) {
+    document.getElementById("theme-style").href = "secondary.css";
+  } else {
+    document.getElementById("theme-style").href = "portest.css";
+  }
+});
